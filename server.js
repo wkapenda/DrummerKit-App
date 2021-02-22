7bee09ae160cc4b33d899262ae6e425626213c4b
WKapenda <wkapenda@gmail.com>
2021-02-22 13:54:06 +0200

    untracked node_modules

--- server.js
+++ server.js
@@ -10,13 +10,6 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname, "public", "index.html")
 })
 
-
-
-
-// app.listen(port, () => {
-//   console.log(`Example app listening at http://localhost:${port}`)
-// })
-
 let port = process.env.PORT;
 if (port == null || port == "") {
   port = 3000;
